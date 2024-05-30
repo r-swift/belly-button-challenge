d7b8d2fa8940a1b4f4df21df17bfa4fee508e59f
r-swift <ricky.swift70@gmail.com>
2024-05-29 19:16:11 -0600

    Fixing Demographic Data box

--- static/js/app.js
+++ static/js/app.js
@@ -18,11 +18,6 @@ function buildMetadata(sample) {
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-    Object.entries(sample).forEach(([key, value]) => {
-      var row = sample_metadata.append("p");
-      row.text(`${key}: ${value}`);
-
-  });
 });
 });
 }
